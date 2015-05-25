472c62b004e683d8a06fa3c30eeffd7a33a23263
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2015-05-25 14:11:53 +0200

    Import OttoCachedField source as a tested base to refactor.

---
+++ src/main/java/com/byoutline/ottocachedfield/OttoCachedField.java
@@ -0,0 +1,70 @@
+package com.byoutline.ottocachedfield;
+
+import com.byoutline.cachedfield.CachedField;
+import com.byoutline.cachedfield.CachedFieldImpl;
+import com.byoutline.cachedfield.ErrorListener;
+import com.byoutline.cachedfield.FieldStateListener;
+import com.byoutline.cachedfield.SuccessListener;
+import com.byoutline.eventcallback.ResponseEvent;
+import com.byoutline.ottocachedfield.internal.ErrorEvent;
+import com.byoutline.ottocachedfield.internal.OttoErrorListener;
+import com.byoutline.ottocachedfield.internal.OttoSuccessListener;
+import com.squareup.otto.Bus;
+
+import javax.inject.Provider;
+
+/**
+ * {@link CachedField} implementation that posts calculated result on Otto bus.
+ *
+ * @param <RETURN_TYPE> Type of cached value.
+ * @author Sebastian Kacprzak <sebastian.kacprzak at byoutline.com>
+ */
+public class OttoCachedField<RETURN_TYPE> extends CachedFieldImpl<RETURN_TYPE> {
+
+    public static Provider<String> defaultSessionIdProvider;
+    public static Bus defaultBus;
+
+    public OttoCachedField(Provider<RETURN_TYPE> valueGetter, ResponseEvent<RETURN_TYPE> successEvent) {
+        this(valueGetter, successEvent, null);
+    }
+
+    public OttoCachedField(Provider<RETURN_TYPE> valueGetter, ResponseEvent<RETURN_TYPE> successEvent, ResponseEvent<Exception> errorEvent) {
+        this(defaultSessionIdProvider, valueGetter, successEvent, errorEvent, defaultBus);
+    }
+
+    public OttoCachedField(Provider<RETURN_TYPE> valueGetter, ResponseEvent<RETURN_TYPE> successEvent, Object errorEvent) {
+        this(defaultSessionIdProvider, valueGetter, successEvent, errorEvent, defaultBus);
+    }
+
+    public OttoCachedField(Provider<String> sessionIdProvider, Provider<RETURN_TYPE> valueGetter, ResponseEvent<RETURN_TYPE> successEvent, ResponseEvent<Exception> errorEvent, FieldStateListener fieldStateListener, Bus bus) {
+        this(sessionIdProvider, valueGetter, successEvent, ErrorEvent.responseEvent(errorEvent), bus);
+    }
+
+    public OttoCachedField(Provider<String> sessionIdProvider, Provider<RETURN_TYPE> valueGetter, ResponseEvent<RETURN_TYPE> successEvent, Object errorEvent, Bus bus) {
+        this(sessionIdProvider, valueGetter, successEvent, ErrorEvent.genericEvent(errorEvent), bus);
+    }
+
+    OttoCachedField(Provider<String> sessionIdProvider, Provider<RETURN_TYPE> valueGetter, ResponseEvent<RETURN_TYPE> successEvent, ErrorEvent errorEvent, Bus bus) {
+        this(sessionIdProvider,
+                valueGetter,
+                new OttoSuccessListener<RETURN_TYPE>(bus, successEvent),
+                new OttoErrorListener(bus, errorEvent),
+                bus);
+    }
+
+    private OttoCachedField(Provider<String> sessionProvider,
+                            Provider<RETURN_TYPE> valueGetter,
+                            SuccessListener<RETURN_TYPE> successHandler, ErrorListener errorHandler, Bus bus) {
+        super(sessionProvider, valueGetter, successHandler, errorHandler);
+        bus.register(valueGetter);
+    }
+
+    public static <RETURN_TYPE> OttoCachedFieldBuilder<RETURN_TYPE> builder() {
+        return new OttoCachedFieldBuilder<RETURN_TYPE>();
+    }
+
+    public static void init(Provider<String> defaultSessionIdProvider, Bus defaultBus) {
+        OttoCachedField.defaultSessionIdProvider = defaultSessionIdProvider;
+        OttoCachedField.defaultBus = defaultBus;
+    }
+}
---
+++ src/main/java/com/byoutline/ottocachedfield/OttoCachedFieldBuilder.java
@@ -0,0 +1,109 @@
+package com.byoutline.ottocachedfield;
+
+import com.byoutline.eventcallback.ResponseEvent;
+import com.byoutline.ottocachedfield.internal.ErrorEvent;
+import com.squareup.otto.Bus;
+
+import javax.inject.Provider;
+
+/**
+ * Fluent interface builder of {@link OttoCachedField}. If you do not like
+ * fluent interface create {@link OttoCachedField} by one of its constructors.
+ *
+ * @param <RETURN_TYPE> Type of object to be cached.
+ * @author Sebastian Kacprzak <sebastian.kacprzak at byoutline.com>
+ */
+public class OttoCachedFieldBuilder<RETURN_TYPE> {
+
+    private Provider<RETURN_TYPE> valueGetter;
+    private ResponseEvent<RETURN_TYPE> successEvent;
+    private ErrorEvent errorEvent;
+    private Provider<String> sessionIdProvider;
+    private Bus bus;
+
+    public OttoCachedFieldBuilder() {
+        bus = OttoCachedField.defaultBus;
+        sessionIdProvider = OttoCachedField.defaultSessionIdProvider;
+    }
+
+    public SuccessEvent withValueProvider(Provider<RETURN_TYPE> valueProvider) {
+        this.valueGetter = valueProvider;
+        return new SuccessEvent();
+    }
+
+    public class SuccessEvent {
+
+        private SuccessEvent() {
+        }
+
+        public ErrorEventSetter withSuccessEvent(ResponseEvent<RETURN_TYPE> successEvent) {
+            OttoCachedFieldBuilder.this.successEvent = successEvent;
+            return new ErrorEventSetter();
+        }
+    }
+
+    public class ErrorEventSetter {
+
+        private ErrorEventSetter() {
+        }
+
+        public CustomSessionIdProvider withGenericErrorEvent(Object errorEvent) {
+            OttoCachedFieldBuilder.this.errorEvent = ErrorEvent.genericEvent(errorEvent);
+            return new CustomSessionIdProvider();
+        }
+
+        public CustomSessionIdProvider withResponseErrorEvent(ResponseEvent<Exception> errorEvent) {
+            OttoCachedFieldBuilder.this.errorEvent = ErrorEvent.responseEvent(errorEvent);
+            return new CustomSessionIdProvider();
+        }
+
+        public OttoCachedField<RETURN_TYPE> build() {
+            OttoCachedFieldBuilder.this.errorEvent = new ErrorEvent(null, null);
+            return OttoCachedFieldBuilder.this.build();
+        }
+    }
+
+    public class CustomSessionIdProvider {
+
+        private CustomSessionIdProvider() {
+        }
+
+        public CustomBus withCustomSessionIdProvider(Provider<String> sessionIdProvider) {
+            OttoCachedFieldBuilder.this.sessionIdProvider = sessionIdProvider;
+            return new CustomBus();
+        }
+
+        public OttoCachedField<RETURN_TYPE> build() {
+            return OttoCachedFieldBuilder.this.build();
+        }
+    }
+
+    public class CustomBus {
+
+        private CustomBus() {
+        }
+
+        public Builder withCustomBus(Bus bus) {
+            OttoCachedFieldBuilder.this.bus = bus;
+            return new Builder();
+        }
+
+        public OttoCachedField<RETURN_TYPE> build() {
+            return OttoCachedFieldBuilder.this.build();
+        }
+    }
+
+    public class Builder {
+
+        private Builder() {
+        }
+
+        public OttoCachedField<RETURN_TYPE> build() {
+            return OttoCachedFieldBuilder.this.build();
+        }
+    }
+
+    private OttoCachedField<RETURN_TYPE> build() {
+        return new OttoCachedField<RETURN_TYPE>(sessionIdProvider, valueGetter, successEvent, errorEvent, bus);
+    }
+}
