3ab56e6de5e19aa4f2525bbacbe581ca959602dc
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2015-08-05 12:33:09 +0200

    Fixed: Abstract Cached field with arg builder returning no arg Cached field.

--- src/main/java/com/byoutline/ibuscachedfield/IBusCachedFieldWithArgBuilder.java
+++ src/main/java/com/byoutline/ibuscachedfield/IBusCachedFieldWithArgBuilder.java
@@ -1,10 +1,12 @@
 package com.byoutline.ibuscachedfield;
 
 import com.byoutline.cachedfield.CachedField;
-import com.byoutline.eventcallback.ResponseEvent;
-import com.byoutline.ibuscachedfield.builders.CachedFieldConstructorWrapper;
-import com.byoutline.ibuscachedfield.internal.ErrorEvent;
+import com.byoutline.cachedfield.CachedFieldWithArg;
+import com.byoutline.cachedfield.ProviderWithArg;
+import com.byoutline.ibuscachedfield.builders.CachedFieldWithArgConstructorWrapper;
+import com.byoutline.ibuscachedfield.events.ResponseEventWithArg;
 
+import javax.annotation.Nullable;
 import javax.inject.Provider;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -19,16 +21,16 @@ import java.util.concurrent.ExecutorService;
  */
 public abstract class IBusCachedFieldWithArgBuilder<RETURN_TYPE, ARG_TYPE, BUS> {
 
-    private final CachedFieldConstructorWrapper<RETURN_TYPE, BUS> constructorWrapper;
-    private Provider<RETURN_TYPE> valueGetter;
-    private ResponseEvent<RETURN_TYPE> successEvent;
-    private ErrorEvent errorEvent;
+    private final CachedFieldWithArgConstructorWrapper<RETURN_TYPE, ARG_TYPE, BUS> constructorWrapper;
+    private ProviderWithArg<RETURN_TYPE, ARG_TYPE> valueGetter;
+    private ResponseEventWithArg<RETURN_TYPE, ARG_TYPE> successEvent;
+    private ResponseEventWithArg<Exception, ARG_TYPE> errorEvent;
     private Provider<String> sessionIdProvider;
     private BUS bus;
     private ExecutorService valueGetterExecutor;
     private Executor stateListenerExecutor;
 
-    public IBusCachedFieldWithArgBuilder(CachedFieldConstructorWrapper<RETURN_TYPE, BUS> constructorWrapper,
+    public IBusCachedFieldWithArgBuilder(CachedFieldWithArgConstructorWrapper<RETURN_TYPE, ARG_TYPE, BUS> constructorWrapper,
                                          BUS defaultBus,
                                          Provider<String> defaultSessionIdProvider,
                                          ExecutorService defaultValueGetterExecutor,
@@ -40,7 +42,7 @@ public abstract class IBusCachedFieldWithArgBuilder<RETURN_TYPE, ARG_TYPE, BUS> 
         stateListenerExecutor = defaultStateListenerExecutor;
     }
 
-    public SuccessEvent withValueProvider(Provider<RETURN_TYPE> valueProvider) {
+    public SuccessEvent withValueProvider(ProviderWithArg<RETURN_TYPE, ARG_TYPE> valueProvider) {
         this.valueGetter = valueProvider;
         return new SuccessEvent();
     }
@@ -50,7 +52,7 @@ public abstract class IBusCachedFieldWithArgBuilder<RETURN_TYPE, ARG_TYPE, BUS> 
         private SuccessEvent() {
         }
 
-        public ErrorEventSetter withSuccessEvent(ResponseEvent<RETURN_TYPE> successEvent) {
+        public ErrorEventSetter withSuccessEvent(ResponseEventWithArg<RETURN_TYPE, ARG_TYPE> successEvent) {
             IBusCachedFieldWithArgBuilder.this.successEvent = successEvent;
             return new ErrorEventSetter();
         }
@@ -61,18 +63,12 @@ public abstract class IBusCachedFieldWithArgBuilder<RETURN_TYPE, ARG_TYPE, BUS> 
         private ErrorEventSetter() {
         }
 
-        public OverrideDefaultsSetter withGenericErrorEvent(Object errorEvent) {
-            IBusCachedFieldWithArgBuilder.this.errorEvent = ErrorEvent.genericEvent(errorEvent);
+        public OverrideDefaultsSetter withResponseErrorEvent(@Nullable ResponseEventWithArg<Exception, ARG_TYPE> errorEvent) {
+            IBusCachedFieldWithArgBuilder.this.errorEvent = errorEvent;
             return new OverrideDefaultsSetter();
         }
 
-        public OverrideDefaultsSetter withResponseErrorEvent(ResponseEvent<Exception> errorEvent) {
-            IBusCachedFieldWithArgBuilder.this.errorEvent = ErrorEvent.responseEvent(errorEvent);
-            return new OverrideDefaultsSetter();
-        }
-
-        public CachedField<RETURN_TYPE> build() {
-            IBusCachedFieldWithArgBuilder.this.errorEvent = new ErrorEvent(null, null);
+        public CachedFieldWithArg<RETURN_TYPE, ARG_TYPE> build() {
             return IBusCachedFieldWithArgBuilder.this.build();
         }
     }
@@ -102,22 +98,21 @@ public abstract class IBusCachedFieldWithArgBuilder<RETURN_TYPE, ARG_TYPE, BUS> 
             return this;
         }
 
-        public CachedField<RETURN_TYPE> build() {
+        public CachedFieldWithArg<RETURN_TYPE, ARG_TYPE> build() {
             return IBusCachedFieldWithArgBuilder.this.build();
         }
     }
-
     public class Builder {
 
         private Builder() {
         }
 
-        public CachedField<RETURN_TYPE> build() {
+        public CachedFieldWithArg<RETURN_TYPE, ARG_TYPE> build() {
             return IBusCachedFieldWithArgBuilder.this.build();
         }
     }
 
-    private CachedField<RETURN_TYPE> build() {
+    private CachedFieldWithArg<RETURN_TYPE, ARG_TYPE> build() {
         return constructorWrapper.build(sessionIdProvider, valueGetter, successEvent, errorEvent, bus,
                 valueGetterExecutor, stateListenerExecutor);
     }
